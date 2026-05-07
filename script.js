3b4351ce4ab083dc7572cdd365efa2e75aae5b10
shriyamcse25-ux <shriyam.cse25@sbjit.edu.in>
2026-05-08 00:44:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function processPayment(){
+
+    alert("Processing payment...");
+
+    setTimeout(function(){
+
+        alert("Payment Successful");
+
+        window.location.href = "#thankyou";
+
+    },1500);
+
+}
+
+function goTop(){
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+}
